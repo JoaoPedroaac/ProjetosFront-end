ded54b0f07ef8f370cf343db055b85206e432fd3
João Pedro Andrade Cintra <joao.cintra7@hotmail.com>
2024-07-22 15:08:13 -0300

    Inicializando projetos feitos em React.
    1.1 React

--- 1.1 Iniciando projeto/src/index.js
+++ 1.1 Iniciando projeto/src/index.js
@@ -8,7 +8,7 @@ import Exemplo from '../src/Componentes/Exemplo';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    < App />
   </React.StrictMode>
 );
 
